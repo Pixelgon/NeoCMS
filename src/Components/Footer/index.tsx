6b6fbd90e8f32205e424c4c3ae8c373171d690c4
Pixelgon <pixelgon@pixelgon.cz>
2024-11-04 13:04:27 +0100

    K cemu backend? 😭

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -65,9 +65,9 @@ export const Footer = () => {
                 <div className="max-w-7xl flex justify-between mx-auto items-center flex-wrap gap-2 py-4 px-reg xl:px-0">
                     <div className={'relative'}>
                         <Link href="/" className={'relative'}>
-                            <Image src="/images/logo/LogoText.svg" fill className={'!relative w-full h-auto'} alt="Logo Pixelgon" priority={false}/>
+                            <Image src="/images/logo/LogoText.svg" fill className={'!relative w-full h-auto max-w-[222px]'} alt="Logo Pixelgon" priority={false}/>
                         </Link>
-                        <p className="m-0 font-light text-xs mt-2">&copy;&nbsp;{date.getFullYear()}&nbsp;| Matěj Matějka | IČO: 21164720</p>
+                        <p className="m-0 font-light text-xs mt-2 w">&copy;&nbsp;{date.getFullYear()}&nbsp;| Matěj Matějka | IČO: 21164720</p>
                     </div>
                     
                     <menu className="flex justify-center items-center list-none gap-4 m-0 mt-4 bg-bg font-light text-xs">
