76f9c5a739981bd2001c0155aa015ae8685c7a67
Pixelgon <pixelgon@pixelgon.cz>
2025-08-07 23:05:08 +0200

    But still need to pregenerate migration.

---
+++ prisma/migrations/20250807210347_deploy/migration.sql
@@ -0,0 +1,47 @@
+-- CreateTable
+CREATE TABLE "public"."Project" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "background" TEXT NOT NULL,
+    "photo" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "createdOn" TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "lastModified" TIMESTAMP(0) NOT NULL,
+    "visible" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Tag" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+
+    CONSTRAINT "Tag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."ProjectTag" (
+    "projectId" TEXT NOT NULL,
+    "tagId" TEXT NOT NULL,
+
+    CONSTRAINT "ProjectTag_pkey" PRIMARY KEY ("projectId","tagId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Project_slug_key" ON "public"."Project"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tag_name_key" ON "public"."Tag"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tag_slug_key" ON "public"."Tag"("slug");
+
+-- AddForeignKey
+ALTER TABLE "public"."ProjectTag" ADD CONSTRAINT "ProjectTag_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "public"."Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."ProjectTag" ADD CONSTRAINT "ProjectTag_tagId_fkey" FOREIGN KEY ("tagId") REFERENCES "public"."Tag"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
