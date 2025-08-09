b8ed8c446d9e7626a02c1aa1cff292bc91e85667
Pixelgon <pixelgon@pixelgon.cz>
2025-08-09 16:03:01 +0200

    Still some design changes.

--- src/components/layout/section.tsx
+++ src/components/layout/section.tsx
@@ -9,7 +9,7 @@ interface SectionProps {
 export const Section: FC<SectionProps> = ({children, isPrim, className}) => { 
     return (
         <section className={`${isPrim ? "bg-bg" : "bg-sec-gradient"} ${className ? className : ''}`}>
-            <div className={`max-w-7xl w-full mx-auto px-reg xl:px-0 flex flex-col gap-7 items-start ${!isPrim ? 'py-14 md:py-28' : 'py-10 md:py-20'}`}>
+            <div className={`max-w-7xl w-full mx-auto px-reg xl:px-0 flex flex-col gap-7 items-start overflow-hidden ${!isPrim ? 'py-14 md:py-28' : 'py-10 md:py-20'}`}>
                 {children}
             </div>
         </section>
